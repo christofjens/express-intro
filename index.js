b953b54414e42305b0605ad3da5154fe3d624293
Christof Jens <christofjens@gmail.com>
2021-05-28 10:49:13 +0200

    Add error handler

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ app.get('/', function (req, res) {
   res.send('hello')
 })
 
+app.get('/error', function (req, res) {
+  throw new Error('Oops I made a mistake')
+})
+
+app.use(function (req, res) {
+  res.status(404).send('Not found')
+})
+
+// Error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send(err.message)
+})
+
 app.listen(3000, function () {
   console.log('App listening on port 3000')
 })
